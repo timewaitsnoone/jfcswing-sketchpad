11b1fc744192bf84ff21c0412a72ed4d3be0a910
cse13261 <cse13261@ns01.cs.yorku.ca>
2013-12-03 17:37:47 -0500

    Implemented popup message, when application window is closed.

--- src/app/components/AppWindow.java
+++ src/app/components/AppWindow.java
@@ -1,6 +1,8 @@
 package app.components;
 
 import java.awt.*;
+import java.awt.event.*;
+
 import javax.swing.*;
 
 /**
@@ -8,7 +10,11 @@ import javax.swing.*;
  */
 public class AppWindow extends JFrame {
 	public AppWindow(JPanel pane) {
-		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); // FIXME 
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE); // FIXME 
+		
+		CloseCheck closeCheck = new CloseCheck();
+		this.addWindowListener(closeCheck);
+		
 		getContentPane().add(new MainToolbar(), BorderLayout.NORTH);
 		getContentPane().add(new DrawingViewport(pane), BorderLayout.CENTER);
 		setMinimumSize(new Dimension(600, 480));
@@ -16,3 +22,16 @@ public class AppWindow extends JFrame {
 		pack();
 	}
 } // AppWindow
+
+class CloseCheck extends WindowAdapter {
+	public void windowClosing(WindowEvent e) {
+		int optionSelected = JOptionPane.showConfirmDialog(null, "Do you want to save changes?", 
+				"Warning!", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.WARNING_MESSAGE);
+		if (optionSelected == 0) {
+			//TODO Call Save Command
+			System.exit(0);
+		} else if (optionSelected == 1) {
+			System.exit(0);
+		}
+	}
+} // CloseCheck
